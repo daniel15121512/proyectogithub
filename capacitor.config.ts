cd5959da1a6cc3c1691ffe8315bd862424be3a00
daniel15121512 <da.ahumadau@duocuc.cl>
2024-10-10 14:46:20 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyectogithub',
+  webDir: 'www'
+};
+
+export default config;
